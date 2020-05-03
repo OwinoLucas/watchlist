607d2fa24994963a5e75b87e40669dd750e5a4b5
Lucas Owino <lucowish35@gmail.com>
2020-05-03 16:09:45 +0300

    add user login and auth

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User,Role
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -13,6 +15,12 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
